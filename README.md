9fc83ee4c77375b7f5c89ba496ecf4547062fa8e
Stephen Osentoski <stosento@umich.edu>
2023-04-01 16:22:41 -0400

    Configured router example

---
+++ README.md
@@ -0,0 +1,7 @@
+# majority.rocks
+
+## Goals
+- [x] Configure tailwind
+- [x] Configure react router
+- [ ] Create main page 
+- [ ] Create login page
--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />}/>
+          <Route path="blogs" element={<Blogs />}/>
+          <Route path="contact" element={<Contact />}/>
+          <Route path="*" element={<NoPage />}/>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
