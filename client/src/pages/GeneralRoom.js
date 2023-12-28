5d49e58c8c6332e69b1910322ec85026dc902082
Stephen Osentoski <stosento@umich.edu>
2023-12-28 10:33:12 -0500

    Change the skip messaging

--- client/src/pages/GeneralRoom.js
+++ client/src/pages/GeneralRoom.js
@@ -67,7 +67,7 @@ const GeneralRoom = ({ socket }) => {
 
             }
             setDisableSkip(false);
-            generateToastMessage('Song has been skipped');
+            generateToastMessage('The room has voted to skip!');
         });
 
     }, []);
